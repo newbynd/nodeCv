5904180c8ef7f2a027c1b8bc7e67bf2f421a6bdf
newbynd <nestorromero.contacto@gmail.com>
2020-04-24 20:25:30 +0200

    Added some dependencies adn refactor of index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
+//REQUISITOS
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const path = require('path'); //para que identifique que S.O. utilizamos y parsee los paths correctos
+const history = require('connect-history-api-fallback');
 const server = express();
 
-//settings
-server.set('port', 3000);
-server.set('views', path.join(__dirname, 'views'));
-//server.set('view engine', 'ejs');
-server.engine('html', require('ejs').renderFile);
+//MIDDLEWARE
+server.use(morgan('tiny'));
+server.use(cors());
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
 
-//middleware
-
-
-//routes
+//ROUTES
 server.use(require('./routes'));
 
-//static files
+//MIDDLEWARE para Vue.js modo History
+server.use(history());
 server.use(express.static(path.join(__dirname, 'public')));
 
-//server listening
+//SETTINGS
+server.set('port', process.env.PORT || 3000);
+server.set('views', path.join(__dirname, 'views'));
 server.listen(server.get('port'), () => {
     console.log('Servidor en puerto ', server.get('port'));
 });
+
+//server.set('view engine', 'ejs');
+server.engine('html', require('ejs').renderFile);
