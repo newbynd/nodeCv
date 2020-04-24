8bcd8224465463432cfa9c9a2bbe168ef5d8e23d
Néstor <nestorromero.contacto@gmail.com>
2020-04-25 00:47:08 +0200

    deleting frontend from server in develop

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
-const express = require('express');
+import express from 'express';
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('index.html', { title: 'Néstor Romero'});
-    //res.sendFile(path.join(__dirname + '/views/index.ejs'));
 });
 
 router.get('/contact', (req, res) => {
     res.render('contact.html', {title: 'Contact me'});
 });
-
 
 module.exports = router;
